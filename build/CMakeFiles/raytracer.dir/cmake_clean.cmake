e65b54dc0a42d4f3dca22d33244e25a85126310c
Vishesh Vigh <vigh.vishesh@gmail.com>
2025-01-15 09:46:23 +0530

    First Commit

---
+++ build/CMakeFiles/raytracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raytracer.dir/main.cpp.obj"
+  "CMakeFiles/raytracer.dir/main.cpp.obj.d"
+  "libraytracer.dll.a"
+  "raytracer.exe"
+  "raytracer.exe.manifest"
+  "raytracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
